60d3d24a4f8d9fd0953e9ec5a1764296022737a3
Jamie Wang <JamieWang@JamiedeMacBook-Pro.local>
2022-09-22 14:25:11 -0300

    This is the first try on task1

--- task1.py
+++ task1.py
@@ -1,15 +1,7 @@
-#Ref: https://stackoverflow.com/questions/22743860/python-fizzbuzz
-#Note I changed the function name too!
 
-def myFizzBuzzSol():
+#This is the first try
+import pandas as pd 
+import numpy as np
 
-	for i in range(1,101):
-		print {
-			3: "FIzz",
-			5: "Buzz",
-			15: "FizzBuz"}.get(15*(not i%15)or 5*(not i%5) or 
-3*(not							 i%3), 
-'{}'.format(i))
-
-myFizzBuzzSol() 
+tl= pd.read_csv("Trail.csv")
 

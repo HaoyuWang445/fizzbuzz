87d0b28c82cf62cb93d0ba1bc7fb2d633425c0be
Jamie Wang <JamieWang@JamiedeMacBook-Pro.local>
2022-09-22 14:10:58 -0300

    task0.py resub

---
+++ task0.py
@@ -0,0 +1,16 @@
+
+
+#Ref: https://stackoverflow.com/questions/22743860/python-fizzbuzz
+#Note I changed the function name too!
+
+def myFizzBuzzSol():
+
+	for i in range(1,101):
+		print {
+			3: "FIzz",
+			5: "Buzz",
+			15: "FizzBuz"}.get(15*(not i%15)or 5*(not i%5) or 
+3*(not							 i%3), 
+'{}'.format(i))
+
+myFizzBuzzSol() 
